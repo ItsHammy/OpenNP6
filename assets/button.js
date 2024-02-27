557234d25974977d0e9f9db5f25aa703d309fd5c
OhItsHammy <hsa85@uclive.ac.nz>
2024-02-27 19:01:58 +1300

    First Commit with Resources

---
+++ assets/button.js
@@ -0,0 +1,12 @@
+function changeImage() {
+    var image = document.getElementById("buttonClick");
+    var src = image.src;
+    var extIndex = src.lastIndexOf(".");
+    var filename = src.substring(0, extIndex);
+    var extension = src.substring(extIndex);
+    image.src = filename + "_i" + extension;
+
+    setTimeout(function() {
+        image.src = src; // Change back to the original source
+    }, 50);
+}
